7aaed36d2613f7f7bec38ae74a06a0c7bab76a83
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-09-03 22:02:17 -0300

    Dividindo componentes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import './App.css'
-import Logo from './componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icones = [perfil,sacola]
+import './App.css';
+import Header from './componentes/Header'
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <ul className='opcoes'>
-          { textoOpcoes.map( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          ) ) }
-        </ul>
-
-        <ul className='icones'>
-          {icones.map((icone) => (
-            <li className='icone'><img src={icone} alt='icones'></img></li>
-          ))}
-        </ul>
-      </header>
-    </div>
-  );
+ return (
+   <div className='App'>
+     <Header />
+   </div>
+ );
 }
-
-export default App;
+export default App
