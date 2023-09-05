2b935d611605caf7ac455267430081f8afa8b466
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-09-04 23:39:48 -0300

    StyledComponents e GlobalStyle

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,26 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`
+
+const IconesContainer = styled.ul`
+  display: flex;
+  align-items: center;
+`
 
 const icones = [perfil,sacola]
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
-          {icones.map((icone) => (
-            <li className='icone'><img src={icone} alt='icones'></img></li>
+        <IconesContainer>
+          { icones.map((icones) => (
+            <Icone><img src={icones}></img></Icone>
           ))}
-        </ul>
+        </IconesContainer>
     )
 }
 
--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,29 @@
-import './estilo.css'
+import styled from 'styled-components'
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
+const OpcaoLi = styled.li`
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
+const OpcoesUl =styled.ul`
+    display: flex;
+`
+
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <OpcoesUl>
             { textoOpcoes.map( (texto) => (
-                <li className='opcao'><p>{texto}</p></li>
+                <OpcaoLi><p>{texto}</p></OpcaoLi>
           ) ) }
-        </ul>
+        </OpcoesUl>
     )
 }
 
