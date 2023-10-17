077d8885f8a920eecca7c6e6651db26a0bdd3e4a
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2023-10-16 22:47:37 -0300

    componentes dinâmicos

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'Liderança em Dados', id: 2, src:livro},
+    {nome: 'JavaScript Assertivo', id: 3, src:livro},
+    {nome: 'Harry Potter', id: 4, src:livro},
+    {nome: 'Senhor do Anéis', id: 5, src:livro},
+]
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from '../Input'
 import styled from "styled-components"
 import { useState } from 'react'
+import{ livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background—image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -23,16 +24,26 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa(){
-    const [textoDigitado, setTextoDigitado] = useState('') 
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]) 
+
     return ( 
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input 
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
-            <p>{textoDigitado}</p>
+            {livrosPesquisados.map(livro => (
+                <div>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src}/>
+                </div>
+            )) }
         </PesquisaContainer>
         
     )
